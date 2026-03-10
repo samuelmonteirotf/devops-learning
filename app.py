bd03db8b5bd3d9ebd61c60d4da10cd0e461e132c
samuelmonteirotf <samuel@monteirotf.com>
2026-03-10 10:12:24 -0300

    feat: add basic math functions

---
+++ app.py
@@ -0,0 +1,11 @@
+def soma(a, b):
+    """Retorna a soma de dois valores."""
+    return a + b
+
+def subtrai(a, b):
+    """Retorna a subtração do primeiro valor pelo segundo."""
+    return a - b
+
+def multiplica(a, b):
+    """Retorna a multiplicação de dois valores."""
+    return a * b
